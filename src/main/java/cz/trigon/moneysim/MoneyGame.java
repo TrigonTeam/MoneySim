b1fbc45614f90e7deb6dec14a0dcd8ced2bd4714
Jaroslav Boudný <jboudny0@gmail.com>
2015-12-17 17:39:24 +0100

    zajebano

--- src/main/java/cz/trigon/moneysim/MoneyGame.java
+++ src/main/java/cz/trigon/moneysim/MoneyGame.java
@@ -5,14 +5,16 @@ import android.opengl.GLES20;
 import java.util.Random;
 
 import cz.trigon.bicepsrendererapi.game.Game;
+import cz.trigon.bicepsrendererapi.gl.impl.render.Renderer;
 
 public class MoneyGame extends Game {
 
-    Random rnd = new Random();
+    private Renderer renderer;
 
     @Override
     public void setup() {
-        GLES20.glClearColor(0, 1, 0, 1);
+        GLES20.glClearColor(0, 0, 0, 1);
+        this.renderer = new Renderer();
     }
 
     @Override
@@ -23,6 +25,12 @@ public class MoneyGame extends Game {
     @Override
     public void renderTick(float ptt) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        this.renderer.vertex(0, 0);
+        this.renderer.vertex(100, 0);
+        this.renderer.vertex(0, 100);
+        this.renderer.flush();
+
     }
 
     @Override
