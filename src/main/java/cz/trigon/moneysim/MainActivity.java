6949e0a1d621ea5626c5e7e8e89dbda88491b922
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2015-12-31 01:04:33 +0100

    Removed unnecessary testing code

--- src/main/java/cz/trigon/moneysim/MainActivity.java
+++ src/main/java/cz/trigon/moneysim/MainActivity.java
@@ -2,18 +2,10 @@ package cz.trigon.moneysim;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
-import java.io.IOException;
-
 import cz.trigon.bicepsrendererapi.game.Surface;
-import cz.trigon.bicepsrendererapi.managers.SoundManager;
-import cz.trigon.bicepsrendererapi.obj.Content;
-import cz.trigon.bicepsrendererapi.obj.Spritesheet;
-import cz.trigon.bicepsrendererapi.obj.Texture;
-
 public class MainActivity extends Activity {
 
     @Override
@@ -28,46 +20,6 @@ public class MainActivity extends Activity {
 
         Surface view = new Surface(this, new MoneyGame());
         setContentView(view);
-
-        Content c = new Content();
-
-        try {
-            c.load();
-        } catch (IOException e) {
-            Log.e(Surface.LDTAG, "Couldn't load content", e);
-        }
-
-        Content c2 = new Content();
-        try {
-            Texture t = c2.get("/default.images/Illuminati.png", Texture.class);
-            Log.d(Surface.LDTAG, t.getSize().x() + "x" + t.getSize().y());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Spritesheet s = c.get("/default.images/spritesheet2.png", Spritesheet.class, false, new Object[] { new String[] { "A", "B" }, null},
-                    new Class[] { String[].class, Content.class });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Log.d(Surface.LDTAG, "Dir count: " + c2.listDirectories("/").size());
-
-        SoundManager s = new SoundManager();
-        try {
-            int m = s.addMusic(c.getDescriptor("/default.music/digeridoo.mp3"));
-            int s1 = s.addSound(c.getDescriptor("/default.sounds/table-hit.mp3"));
-            int s2 = s.addSound(c.getDescriptor("/default.sounds/flame-ignition.wav"));
-
-            s.playMusic(m);
-            s.playSound(s1);
-            s.playSound(s2);
-            s.stopMusic();
-            //s.resumeMusic();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
--- src/main/java/cz/trigon/moneysim/MoneyGame.java
+++ src/main/java/cz/trigon/moneysim/MoneyGame.java
@@ -2,22 +2,38 @@ package cz.trigon.moneysim;
 
 import android.opengl.GLES20;
 
+import java.io.IOException;
 import java.util.Random;
 
 import cz.trigon.bicepsrendererapi.game.Game;
+import cz.trigon.bicepsrendererapi.obj.Content;
+import cz.trigon.bicepsrendererapi.obj.SoundEffect;
 
 public class MoneyGame extends Game {
 
-    Random rnd = new Random();
+    private Random rnd = new Random();
+    private Content content;
+    private SoundEffect se;
 
     @Override
     public void setup() {
         GLES20.glClearColor(0, 1, 0, 1);
+
+        try {
+            this.content = new Content();
+            this.content.load();
+            this.se = this.content.get("/default.sounds/table-hit.mp3", SoundEffect.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void tick() {
-
+    public void tick(int ticks) {
+        if (ticks % (60 * 4) == 0) {
+            se.play(rnd.nextFloat());
+            GLES20.glClearColor(rnd.nextFloat(), rnd.nextFloat(), rnd.nextFloat(), 1f);
+        }
     }
 
     @Override
